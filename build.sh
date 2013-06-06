ee0f0153063e25c0d3ef3eb1e98717e4eb4e5f82
Casper B. Hansen <casperbhansen@me.com>
2013-06-06 17:57:05 +0200

    Added utility shell scripts.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#
+# Builds the PDF
+#
+
+pdflatex disposition.tex
+
---
+++ clean.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#
+# Cleans up the working directory. Useful when references mess up.
+#
+
+rm -r *.{aux,log,out,pdf,toc}
+
